adc72e252242ba7068c9ab6ffb749c2336588bf3
rmazzoli <rafael.mazzoli@hotmail.com>
2019-10-24 22:42:32 -0300

    Add PostList

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Heaeder'
 
-function App(){
-  return <Header />;
+import Header from './components/Header';
+import PostList from './components/PostList';
+
+
+function App() {
+  return (
+    <>
+      <Header />
+      <PostList />
+    </>  
+    
+  );
 }
 
 export default App;
--- src/components/Heaeder.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function Header(){
-  return(<div class="header">Facebook</div>);
-}
-
-export default Header;
